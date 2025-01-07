28cfc0d0d554e7566dc5edc55ce8db000e2a8079
BB <balrajbains@sky.com>
2025-01-07 15:03:19 +0000

    update email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,9 @@ const Contact = () => {
           <Fade>
             <h1>Contact</h1>
           </Fade>
-          <h2 className="email-link">&#98;&#108;&#114;&#97;&#106;&#99;&#111;&#110;&#116;&#97;&#99;&#116;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</h2>
+          <h2 className="email-link"> Drop an email at <a className="email-link" href= "mailto: hello@blraj.com">hello@blraj.com</a>
+          </h2>
+          
           {/* <h3>{yourData.contactSubHeading}</h3> */}
           <div className="social-icons">
             {yourData.social.map((socialLink, index) => (
